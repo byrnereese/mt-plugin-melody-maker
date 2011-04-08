dd0d4ed37086d2c294a573a16c2e10528299524a
Byrne Reese <byrne@majordojo.com>
2011-04-08 00:51:16 -0700

    Initial implementation of an ajax blog search function embedded within the blog selector.

--- plugins/MelodyMaker/static/js/melody_ui_func.js
+++ plugins/MelodyMaker/static/js/melody_ui_func.js
@@ -76,6 +76,33 @@
             'dateFormat' : 'yy-mm-dd' 
         });
 
+        /* Blog Selector */
+        $('#search-sites').ajaxForm({
+            dataType:'json',
+            error: function(data,msg,error) {
+                alert('There was an error processing the results: ' + msg + ', error=' + error);
+                var ul = $('#site_search_results').show();
+            },
+            beforeSubmit: function() {
+                $('#site_search_results_indicator').remove();
+                var ul = $('#site_search_results').fadeOut();
+                $('<div id="site_search_results_indicator" class="spinner">Spinning</div>').insertAfter(ul);
+            },
+            success: function(data) {
+                var blogs = data['result'];
+                var indicator = $('#site_search_results_indicator');
+                if (blogs.length == 0) {
+                    indicator.attr('class','no_results').html('No blogs found.');
+                } else {
+                    indicator.remove();
+                    var ul = $('#site_search_results').empty().show();
+                    jQuery.each(blogs, function( index, blog ) { 
+                        $('<li class="site result"><a href="'+ScriptURI+'?blog_id='+blog['blog.id']+'&amp;__mode=dashboard"><img src="'+StaticURI+'support/plugins/melodymaker/images/site_search/ico_default_logo.png"></a><div class="result_data"><a class="pin" href="#">Pin This Site</a><h4 class="site_title"><a href="'+ScriptURI+'?blog_id='+blog['blog.id']+'&amp;__mode=dashboard">'+blog[ 'blog.name' ]+'</a></h4><a class="site_preview" rel="external" href="'+blog['blog.site_url']+'">'+blog['blog.site_url']+'</a></div></li>').hide().appendTo( ul ).fadeIn();
+                    });
+                }
+            }
+        });
+
 	});
 
 })(jQuery);
