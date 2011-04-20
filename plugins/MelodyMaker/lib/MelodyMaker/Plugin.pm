5c3f2e76438e29079d484068bde33902b391eeaf
Byrne Reese <byrne@majordojo.com>
2011-04-20 11:03:01 -0700

    Allowing plugin to be invoked during upgrade.

--- plugins/MelodyMaker/lib/MelodyMaker/Plugin.pm
+++ plugins/MelodyMaker/lib/MelodyMaker/Plugin.pm
@@ -17,8 +17,8 @@ sub init_request {
     my $cb     = shift;
     my $plugin = $cb->plugin;
     my ($app)  = @_;
-    return if $app->id ne 'cms';
-    return unless $app->isa('MT::App::CMS');
+    return if $app->id !~ /(cms|wizard|upgrade)/;
+    return unless $app->isa('MT::App::CMS') || $app->isa('MT::App::Upgrader') || $app->isa('MT::App::Wizard');
     return if $app->query->param('noui');
     $app->config('AltTemplatePath', $plugin->path . '/tmpl');
     return 1;
